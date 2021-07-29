5da6f87223d8c47ec3cec0b870359627df25ec89
nvc8996 <nvc.8996@gmail.com>
2021-07-29 20:50:52 +0300

    added btvn 2

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Javascript file for challenging part
+const containers = Array.from(document.getElementsByClassName('imgcontainer'));
+const modal = document.getElementById('modal-plain');
+const modalImage = document.getElementById('modal-img');
+const closeButton = document.getElementById('close-btn');
+
+containers.forEach(container => {
+    container.onclick=  (e) => {
+        modal.style.display = 'block';
+        modalImage.src = e.target.src;
+    };
+});
+
+closeButton.onclick = () => {
+    modal.style.display = 'none';
+}
+
+modal.onclick = (e) => {
+    if (e.target === modalImage) {
+        return;
+    }
+    modal.style.display = 'none';
+}
